36a71a2157c114aeeeff3ef43dc51ff9a16dc625
SHRYITHA SUDDALA <2303a51637@sru.edu.in>
2026-03-10 10:02:13 +0530

    Add cart functionality and tests

---
+++ test.js
@@ -0,0 +1,16 @@
+const cart = require('./Cart');
+const payment = require('./payment');
+
+// Testing cart functionality
+console.log("Testing cart functionality...");
+cart.addItemToCart('Apple');
+cart.addItemToCart('Banana');
+cart.showCart();
+cart.removeItemFromCart('Apple');
+cart.showCart();
+
+// Testing payment functionality
+console.log("Testing payment functionality...");
+payment.validatePayment('Credit Card');
+payment.validatePayment('PayPal');
+payment.validatePayment('Bitcoin');  // Unsupported payment method
